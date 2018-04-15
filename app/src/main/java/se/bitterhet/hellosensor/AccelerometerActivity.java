9962bef0c3b5cefbe16566382020ec72e543499b
Niklas Lindvall <nic.kingsblood@gmail.com>
2018-04-15 20:13:05 +0200

    Implemented Orientation in AccelerometerActivity
    Also upgraded android studio and formatted the whole project
    with codestyle.

--- app/src/main/java/se/bitterhet/hellosensor/AccelerometerActivity.java
+++ app/src/main/java/se/bitterhet/hellosensor/AccelerometerActivity.java
@@ -13,7 +13,12 @@ import android.widget.TextView;
 public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
+
     private Sensor mAccelerometer;
+    private float[] mLastAccData;
+
+    private Sensor mMagnetometer;
+    private float[] mLastMagData;
 
 
     private TextView mXTextView;
@@ -34,13 +39,15 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         mDirectionTextView = findViewById(R.id.text_direction);
 
 
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
 
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     protected void onPause() {
@@ -49,20 +56,57 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
     public void onSensorChanged(SensorEvent event) {
-      if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float[] values = event.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-            mXTextView.setText("X: " + x);
-            mYTextView.setText("Y: " + y);
-            mZTextView.setText("Z: " + z);
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mLastMagData = event.values;
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mLastAccData = event.values;
         } else {
-            Log.e("Nic","Unknown senor: " + event.sensor.getType());
+            Log.e("Nic", "Unknown senor: " + event.sensor.getType());
         }
+
+        if (mLastAccData != null) {
+            updateAccelerometerViews();
+
+        }
+        if (mLastAccData != null && mLastMagData != null) {
+            float roll = calculatePitch();
+            updatePitchView(roll);
+        }
+    }
+
+    private float calculatePitch() {
+        float R[] = new float[9];
+        float I[] = new float[9];
+        boolean success = SensorManager.getRotationMatrix(R, I, mLastAccData, mLastMagData);
+        if (success) {
+            float orientation[] = new float[3];
+            SensorManager.getOrientation(R, orientation);
+            // [0] Azimuth, angle of rotation about the -z axis.
+            // [1] Pitch, angle of rotation about the x axis
+            // [2] Roll, angle of rotation about the y axis
+            return orientation[2];
+        } else {
+            return 0;
+        }
+    }
+
+    private void updatePitchView(float roll) {
+        if (roll < 0) {
+            mDirectionTextView.setText("Left");
+        } else {
+            mDirectionTextView.setText("Right");
+        }
+    }
+
+    private void updateAccelerometerViews() {
+        float x = mLastAccData[0];
+        float y = mLastAccData[1];
+        float z = mLastAccData[2];
+        mXTextView.setText("X: " + x);
+        mYTextView.setText("Y: " + y);
+        mZTextView.setText("Z: " + z);
     }
 }
